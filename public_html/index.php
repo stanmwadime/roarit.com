6c6fa28bb6a6454bce5ff68b3d8ef94c48e38008
stanmwadime <stanley@gmail.com>
2023-01-21 15:02:09 +0300

    initial commit

--- public_html/index.php
+++ public_html/index.php
@@ -3,70 +3,7 @@
     <head>
         <meta charset="UTF-8"/>
         <title>ROAR IT</title>
-        <link rel="stylesheet" type="text/css" href="/styles.css">
-        <style>
-            *{
-    margin: 20px;
-    padding:0;
-}
-body{
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    font-size: 15;
-    line-height: 1.5;
-    background: color #a02323;
-}
-#container{
-    background-color: #8f8989;
-    margin: 50px auto;
-    overflow: auto;
-    width: 60%;
-    min-height: 630px;
-}
-header h1{
-    color:#ffffff;
-    font-size:32px;
-    padding: 15px 0 10px 10px;
-    border-bottom: 1px solid #ffffff;
-}
-.roars{
-    width: 90%;
-    background-color: #f4f4f4;
-    height:400px;
-    margin:70px auto;
-    overflow: auto;
-}
-.roar{
-    list-style:none;
-    padding: 8px;
-    border-bottom: 1px #cccc dotted;
-}
-.input{
-    width:90%;
-    min-height: 80px;
-    margin:auto;
-    padding: 0;
-}
-#input input[type="text"]{
-    height: 25px;
-    width:70%;
-    length: 100%;
-    padding:3px;
-    margin-bottom:20px;
-    border:#666666 solid 1px;
-}
-#input .roar-btn(
-    padding:5px;
-    margin-bottom:30px;
-    width:100%;
-    margin:10px auto;
-)
-
-@media only screen and (max-width:768px){
-    #input input [type='text']{
-        width:98;
-    }
-}
-        </style>
+        <link rel="stylesheet" type="text/css" href="/css/styles.css">
     </head>
     <body>
         <div id="container">
